27f4b3d18d404c28ebbf2e8112e554fdd8593d27
Prashanth Padmanabh <prashanth128@gmail.com>
2013-05-16 13:28:32 -0400

    miscellaneous js file - strange regex

---
+++ google-js-code-collection/misc.js
@@ -0,0 +1,2 @@
+// regular expression of all characters I've seen.
+Zla = RegExp("^[^A-Za-z\u00c0-\u00d6\u00d8-\u00f6\u00f8-\u02b8\u0300-\u0590\u0800-\u1fff\u2c00-\ufb1c\ufe00-\ufe6f\ufefd-\uffff]*[\u0591-\u07ff\ufb1d-\ufdff\ufe70-\ufefc]")
